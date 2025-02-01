afa11154a1d16b538f8861d690e7b851fdfc983c
William <willcburroughs@gmail.com>
2025-02-01 17:45:07 -0600

    Finished first version Desktop

---
+++ src/components/molecules/DesktopFooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../index.css';
+import FooterDesktopImage from '../../images/FooterDesktop.png';
+
+function DesktopFooter() {
+  return (
+    <div className='BottomFooter'>
+        <img src= {FooterDesktopImage}/>
+    </div>
+  );
+}
+
+export default DesktopFooter;
