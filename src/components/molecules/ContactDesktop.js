ef5f3fec661fd3fb258978497b855891965d65d8
William <willcburroughs@gmail.com>
2025-02-01 12:30:36 -0600

    Adding correct form input format

--- src/components/molecules/ContactDesktop.js
+++ src/components/molecules/ContactDesktop.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import '../../index.css';
-
+import FormElement from '../atoms/FormElement';
 
 function ContactDesktop() {
   return (
     <div className='ContactDesktop'>
         <div className='HoldFormRow'>
-
+            <FormElement placeholder = "Hello"/>
+            <FormElement/>
+            <FormElement/>
         </div>
         <div className='HoldFormRow'>
-            
+            <FormElement/>
+            <FormElement/>
+            <FormElement/>
         </div>
         <div className='HoldFormRow'>
 
