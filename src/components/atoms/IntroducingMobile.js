e100deb3368c97afbb0ec143d828b6848846aee8
William <willcburroughs@gmail.com>
2025-02-12 21:24:56 -0500

    Adding in new component for introducing section

---
+++ src/components/atoms/IntroducingMobile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../index.css'
+
+function Introducing() {
+  return (
+    <div className='centerAlignIntroducing'>
+      <h3 className='introducing' id='introducingMobile'> • Introducing •</h3>
+    </div>
+  );
+}
+
+export default Introducing;
