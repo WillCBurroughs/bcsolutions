76ec923f65f16dc1e026422ea6b1eef42022d6c7
William <willcburroughs@gmail.com>
2025-05-09 18:41:57 -0400

    Adding mobile header area

---
+++ src/components/atoms/CreateMobile.js
@@ -0,0 +1,10 @@
+import mobileImage from '../../images/CreateMobile.png'
+import '../../index.css'
+
+function CreateMobile() {
+  return (
+    <img src = {mobileImage} width = {"100%"} className='CreateMobile' alt="" />
+  );
+}
+
+export default CreateMobile;
