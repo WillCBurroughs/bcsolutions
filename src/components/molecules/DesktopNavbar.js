37a9a7362f398b59f1ac7537faeb16c34307f034
William <willcburroughs@gmail.com>
2025-10-25 10:39:13 -0500

    adding privacy policy to website

--- src/components/molecules/DesktopNavbar.js
+++ src/components/molecules/DesktopNavbar.js
@@ -9,10 +9,10 @@ function DesktopNavbar() {
       <div class="logo"><LogoNavbar/></div>
     </div>
     <div class="navbar-center">
-      <a href="#introducing"><HeaderNavbar headerText="home" /></a>
-      <a href="#services"><HeaderNavbar headerText="services"/></a>
-      <a href="#value"><HeaderNavbar headerText="value" /> </a>
-      <a href="#team"><HeaderNavbar headerText="about" /> </a>
+      <a href="#introducing"><HeaderNavbar headerText="Home" /></a>
+      <a href="#services"><HeaderNavbar headerText="Services"/></a>
+      <a href="#value"><HeaderNavbar headerText="Value" /> </a>
+      <a href="#team"><HeaderNavbar headerText="About" /> </a>
     </div>
     <div class="navbar-right">
       <a href="#ContactPlace"><div class="contact-btn">Contact us</div></a>
